5862fd386941a940d0d6e1149de09d6a96fd5c69
DarkSiderOC <darksider96@gmail.com>
2018-03-06 13:02:06 +0100

    introdotta query per elasticsearch

--- Code/stabHavana/server/routes/example.js
+++ Code/stabHavana/server/routes/example.js
@@ -8,4 +8,26 @@ export default function (server) {
     }
   });
 
+  /*
+    1) cluster.state, controlla le API per vedere cosa puoi chiedere
+  */
+  
+   /*
+  server.route({
+    path: '/api/v0/stabHavana/retrieveData',
+    method: 'GET',
+    handler(req, reply) {
+      server.plugins.elasticsearch.callWithRequest(req, 'cluster.state', {
+        metric: 'metadata',
+        index: req.params.name
+      }).then(function (response) {
+        reply(response.metadata.indices[req.params.name]);
+      });
+    }
+  });
+  /*
+  
+  
+  
+  
 }
